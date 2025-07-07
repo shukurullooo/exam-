544c8c6698ebd65417e6e307a5aa75f6cf0cbca2
shukurullooo <sheykh094@gmail.com>
2025-07-07 10:08:10 +0500

    salom

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,12 @@
 import { useMovie } from '@/api/hooks/useMovie'
 import React from 'react'
 import MovieView from '@/components/movie-view/MovieView'
-import  Carousel  from '../../components/carusel/Carusel'
 
 const Home = () => {
   const {getMovies} = useMovie()
   const {data} = getMovies({page: 1, without_genres: "18,36,27,10749"})
   return (
     <div>
-     <Carousel/>
       <MovieView data={data?.results?.slice(0, 8)}/>
     </div>
   )
